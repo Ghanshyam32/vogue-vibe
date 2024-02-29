21058a64a82a07ee032988f1ca13647cb178da7a
Ghanshyam32 <ghanshyammishra3205615@gmail.com>
2024-03-01 01:01:54 +0530

    initial commit

---
+++ app/src/main/java/com/ghanshyam/voguevibe/activities/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.ghanshyam.voguevibe.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ghanshyam.voguevibe.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
---
+++ app/src/main/java/com/ghanshyam/voguevibe/fragments/loginRegister/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.ghanshyam.voguevibe.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.ghanshyam.voguevibe.R
+
+class LoginFragment: Fragment(R.layout.fragment_login) {
+}
